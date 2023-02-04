81012d63f844c418119acb5c8dbe3a19aa97be03
Laurin-Notemann <laurin.notemann@gmail.com>
2023-02-04 16:58:58 +0100

    FUCK GOOGLE

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,71 @@
-import { StyleSheet } from 'react-native';
+import * as React from "react"
+import * as WebBroswer from "expo-web-browser"
+import * as Google from "expo-auth-session/providers/google"
+
+import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
+WebBroswer.maybeCompleteAuthSession()
+
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+    const [accessToken, setAccessToken] = React.useState(null)
+    const [user, setUser] = React.useState(null)
+    const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+        clientId: "614417646190-dbl1mao4r8bcjmam2cmcgtfo4c35ho1h.apps.googleusercontent.com",
+        iosClientId: "614417646190-vcu5a3ini5nnr0elfaqt8fprs358mp2i.apps.googleusercontent.com",
+        androidClientId: "614417646190-hhupm8k97a22rvv2gfdcoqi1gus8qunq.apps.googleusercontent.com"
+    })
+     
+    React.useEffect(() => {
+        if (response?.type === "success"){
+            setAccessToken(response.authentication.accessToken)
+            accessToken && fetchUserInfo()
+        }
+    }, [response, accessToken])
+
+    async function fetchUserInfo(){
+        let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).catch((error) => {
+            console.log(accessToken)
+            console.log(error)
+        })
+        const userInfo = await response.json()
+        setUser(userInfo)
+    }
+
+    const ShowUserInfo = () => {
+        if (user) {
+            return (
+                <View>
+                    <Text>Sack</Text>
+                    <Text>{user.name}</Text>
+                </View>
+            )
+        }
+    }
+
+    
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View>
+    {user && <ShowUserInfo />}
+    {user === null && 
+        <TouchableOpacity
+        disabled ={!request}
+        onPress={() => {
+            promptAsync()
+        }}
+        >
+            <Text>Nacken</Text>
+        </TouchableOpacity>
+    }
     </View>
+    
   );
 }
 
