afceefdc064be4f6fdbeded7a200db9f8f0bb380
Jannis <114334799+kirrj@users.noreply.github.com>
2023-02-05 01:51:33 +0100

    dings so dass es klappt und so aber hab ich leider forgor death skull emoji

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -16,7 +16,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
     //const [accessToken, setAccessToken] = React.useState(null)
     const [authState, setAuthState] = React.useState<AuthSession.TokenResponse | null>(null)
     const [tokenResponse, setTokenResponse] = React.useState<AuthSession.TokenResponse | null>(null)
-    Google.useAuthRequest()
+
     const [user, setUser] = React.useState(null)
     const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
         clientId: "614417646190-dbl1mao4r8bcjmam2cmcgtfo4c35ho1h.apps.googleusercontent.com",
@@ -26,30 +26,12 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
     })
 
     async function ensureAuth() {
-        // const nowTime = Math.floor(Date.now() / 1000)
-
-        // console.log(nowTime)
-        // console.log(authState?.expiresAt)
-
-        // if (nowTime > authState?.expiresAt!) {
-        //     //hoppps brauchen wa so neudings
-        //     const refreshResult = new AuthSession.RefreshTokenRequest({
-        //         refreshToken: authState?.refreshToken,
-        //         clientId: "614417646190-dbl1mao4r8bcjmam2cmcgtfo4c35ho1h.apps.googleusercontent.com"
-        //     }).performAsync(Google.discovery);
-
-        //     console.log(refreshResult)
-
-
-        // }
-
-        console.log("SACHE WIRD REFRESHIERT!!!!!!!!!!!1111!")
-
+        console.log("refresh check")
         if (authState?.shouldRefresh()) {
+            console.log("SACHE WIRD REFRESHIERT!!!!!!!!!!!1111!")
             //lol wenn lol
             setAuthState(await authState.refreshAsync({
-                clientId: "614417646190-dbl1mao4r8bcjmam2cmcgtfo4c35ho1h.apps.googleusercontent.com",
-
+                clientId: "614417646190-dbl1mao4r8bcjmam2cmcgtfo4c35ho1h.apps.googleusercontent.com"
             }, Google.discovery))
         }
     }
@@ -58,37 +40,36 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
         if (response?.type === "success") {
             console.log(response.authentication)
             setAuthState(response.authentication)
-            // setAuthState({
-            //     accessToken: response.authentication?.accessToken!,
-            //     expiresAt: response.authentication?.issuedAt! + response.authentication?.expiresIn!,
-            //     refreshToken: response.authentication?.refreshToken!,
-            //     sache: response.authentication
-            // })
+
             if (authState?.accessToken != null) fetchUserInfo()
         }
     }, [response])
 
     async function fetchUserInfo() {
+        console.log("at1", authState?.accessToken)
+        await ensureAuth()
+        console.log("at2", authState?.accessToken);
         let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
             headers: {
-                Authorization: `Bearer ${accessToken}`
+                Authorization: `Bearer ${authState?.accessToken}`
             }
         }).catch((error) => {
-            console.log(accessToken)
+            console.log(authState?.accessToken)
             console.log(error)
         })
         //console.log("JANNES SACK\n\n\n", await response.json())
 
-        let response2 = await fetch("https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest", {
+        let response2 = await fetch("https://www.googleapis.com/calendar/v3/users/me/calendarList", {
             headers: {
-                Authorization: `Bearer ${accessToken}`
+                Authorization: `Bearer ${authState?.accessToken}`
             }
         }).catch((error) => {
-            console.log(accessToken)
+            console.log(authState?.accessToken)
             console.log(error)
         })
-        console.log(JSON.stringify(await response2.json()))
-        const userInfo = JSON.stringify(await response.json())
+        const userInfo = await response.json();
+        console.log("usersache", JSON.stringify(userInfo))
+        console.log("calandAR", JSON.stringify(await response2.json()))
 
         setUser(userInfo)
     }
@@ -98,10 +79,12 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
             return (
                 <View>
                     <Text>Sack</Text>
-                    <Text>{user.name}</Text>
+                    <Text>{user?.name}</Text>
                 </View>
             )
         }
+
+        return null
     }
 
 
